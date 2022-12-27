1dd442d94c591ace6beedccbe21bd2f0f68e3cc0
kimheeseok <kim1140129@gmail.com>
2022-12-27 15:30:06 +0900

    <Fix> 사용하지 않응 코드 제거

--- src/app.js
+++ src/app.js
@@ -16,22 +16,6 @@ const {
 const morgan = require('morgan');
 const Winston = require('./util/WinstonUtil.js');
 
-const session = require('express-session');
-const fileStore = require('session-file-store')(session);
-
-app.use(session({
-  httpOnly: true,	//자바스크립트를 통해 세션 쿠키를 사용할 수 없도록 함
-  secure: true,	//https 환경에서만 session 정보를 주고받도록 처리
-  secret: 'secret key',	//암호화하는 데 쓰일 키
-  resave: false,	//세션을 언제나 저장할지 설정함
-  saveUninitialized: true,	//세션이 저장되기 전 uninitialized 상태로 미리 만들어 저장
-  cookie: {	//세션 쿠키 설정 (세션 관리 시 클라이언트에 보내는 쿠키)
-    httpOnly: true,
-    secure: true
-  }
-}));
-
-
 app.use(
     morgan(':method :status :url :response-time ms', { stream: Winston.stream })
 );
