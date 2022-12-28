f87b8d3a8eb357990a843dd961e1383063ae8d3c
DY <p930601@gmail.com>
2022-12-28 21:51:13 +0700

    initialize

--- src/main/contents/contents.repository.js
+++ src/main/contents/contents.repository.js
@@ -19,7 +19,14 @@ class ContentsRepository extends Contents {
   // 컨텐츠 상세 정보 조회 API
   getOneMovie = async (contentsId) => {
     try {
-      return await Contents.findByPk(contentsId);
+      // return await Contents.findByPk(contentsId);
+      return Contents.findAll({
+        where: { contentsId },
+        attributes: ['contentsId', 'title', 'summary', 'rating', 'genre', 'release', 'director', 'actor', 'isNetflixOriginal',
+          'trailerUrl', 'thumbnailUrl',],
+        raw: true
+
+      })
     } catch (error) {
       throw error;
     }
@@ -27,29 +34,29 @@ class ContentsRepository extends Contents {
 
 
 
-getRating = async (rating) => {
-  return Contents.findAll({
-  where : {rating},
-  attributes : ['contentsId','title', 'summary', 'rating','genre', 'release','director','actor','isNetflixOriginal' ,
-    'trailerUrl' ,'thumbnailUrl' ,],
-     raw : true
-  
-  
-  });
- }
+  getRating = async (rating) => {
+    return Contents.findAll({
+      where: { rating },
+      attributes: ['contentsId', 'title', 'summary', 'rating', 'genre', 'release', 'director', 'actor', 'isNetflixOriginal',
+        'trailerUrl', 'thumbnailUrl',],
+      raw: true
 
- getGenre = async(genre) => {
-  return Contents.findAll({
-    where : {genre},
-    attributes : ['contentsId','title', 'summary', 'rating','genre', 'release','director','actor','isNetflixOriginal' ,
-    'trailerUrl' ,'thumbnailUrl' ,],
-     raw : true
 
-  })
+    });
+  }
 
- }
+  getGenre = async (genre) => {
+    return Contents.findAll({
+      where: { genre },
+      attributes: ['contentsId', 'title', 'summary', 'rating', 'genre', 'release', 'director', 'actor', 'isNetflixOriginal',
+        'trailerUrl', 'thumbnailUrl',],
+      raw: true
 
-} 
+    })
+
+  }
+
+}
 
 
 
